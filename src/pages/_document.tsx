498c9977e9c1b928ac190d1eaacf4fb5e36a090d
Mihai <mihai.stefanuti@wirtek.com>
2023-04-26 16:03:43 +0200

    Added a dropdown in the Navbar

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,10 +5,12 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="shortcut icon" href="/LDU-logo-light.png" />
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet" />
         </Head> 
       <body>
         <Main />
         <NextScript />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
       </body>
     </Html>
   )
